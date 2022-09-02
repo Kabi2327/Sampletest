25227d430dffeb009cbd2597fc9afa54b6a4b82a
Kabi2327 <kabimadhan@gmail.com>
2022-09-02 20:47:42 +0530

    trial

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,61 @@
+package org.emp;
+
+public class Employee {
+
+	private void empId() {
+		System.out.println("101213");
+	}
+	
+	private void empName() {
+		System.out.println("Kabilash");
+	}
+	
+	private void empDob() {
+		System.out.println("18.12.1994");
+	}
+	private void empPhone() {
+		System.out.println("9003912698");
+	}
+	
+	
+	private void empEmail() {
+		System.out.println("kabimadhangmail.com");
+	}
+	
+	
+	private void empAddress() {
+		System.out.println("Kumbakonam");
+	}
+	
+	public static void main(String[] args) {
+		
+	
+		 Employee a = new Employee();
+		 
+		a.empId();
+		a.empName();
+		a.empDob();
+		a.empPhone();
+		a.empEmail();
+		a.empAddress();
+		
+				
+
+		
+	}
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
